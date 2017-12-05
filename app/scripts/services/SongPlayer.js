e8f9405b2732f595f28d25c0b60861a4cb9cc936
Deanna Swallow <swallowdeanna@gmail.com>
2017-12-04 17:31:42 -0700

    CP7 Services2 Completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,14 +1,49 @@
 (function(){
-  function SongPlayer(){
+  function SongPlayer() {
     var SongPlayer = {};
+    var currentSong = null;
 
-    SongPlayer.play = function(song){
-      var currentBuzzObject = new buzz.sound(song.audioUrl, {
+    /**
+    *@desc Buzz object audio file
+    *@type {Object}
+    */
+    var currentBuzzObject = null;
+
+    /**
+    *@function setSong
+    *@desc Stops currently playing song and loads new audio file as
+    *currentBuzzObject.
+    *@param {Object} song
+    */
+    var setSong = function(song) {
+      if(currentBuzzObject) {
+        currentBuzzObject.stop();
+        currentSong.playing = null;
+      }
+
+      currentBuzzObject = new buzz.sound(song.audioUrl, {
         formats: ['mp3'],
         preload: true
       });
 
-      currentBuzzObject.play();
+      currentSong = song;
+    };
+
+    SongPlayer.play = function(song) {
+      if(currentSong !== song) {
+        setSong(song);
+        currentBuzzObject.play();
+        song.playing = true;
+      } else if (currentSong === song) {
+        if(currentBuzzObject.isPaused()) {
+          currentBuzzObject.play();
+        }
+      }
+    };
+
+    SongPlayer.pause = function(song) {
+      currentBuzzObject.pause();
+      song.playing = false;
     };
 
     return SongPlayer;
