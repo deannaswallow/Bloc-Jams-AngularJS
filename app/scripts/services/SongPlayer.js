5b6d989f9ed7f2402fde2be352ba72a2e8d5ea15
Deanna Swallow <swallowdeanna@gmail.com>
2017-12-05 10:09:52 -0700

    Assignment 7 Services2 Completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,6 +1,15 @@
 (function(){
   function SongPlayer() {
+    /**
+    *@desc SongPlayer object that service returns
+    *@type {Object}
+    */
     var SongPlayer = {};
+
+    /**
+    *@desc Current song Object
+    *@type {Object}
+    */
     var currentSong = null;
 
     /**
@@ -29,11 +38,25 @@
       currentSong = song;
     };
 
+    /**
+    *@function playSong
+    *@desc Starts playing selected song and sets playing property to true.
+    *@param {Object} song
+    */
+    var playSong = function(song){
+      currentBuzzObject.play();
+      song.playing = true;
+    };
+
+    /**
+    *@function SongPlayer.play
+    *@desc Plays selected song or paused song
+    *@param {Object} song
+    */
     SongPlayer.play = function(song) {
       if(currentSong !== song) {
         setSong(song);
-        currentBuzzObject.play();
-        song.playing = true;
+        playSong(song);
       } else if (currentSong === song) {
         if(currentBuzzObject.isPaused()) {
           currentBuzzObject.play();
@@ -41,6 +64,11 @@
       }
     };
 
+    /**
+    *@function SongPlayer.pause
+    *@desc Pauses selected song if playing and sets playing property to false
+    *@param {Object} song
+    */
     SongPlayer.pause = function(song) {
       currentBuzzObject.pause();
       song.playing = false;
