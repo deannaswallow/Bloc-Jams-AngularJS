e271725cb3f31c8ac017f6a61c8ed315503470ad
Deanna Swallow <swallowdeanna@gmail.com>
2017-12-06 11:18:07 -0700

    CP8 Services3 Completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,16 +1,10 @@
 (function(){
-  function SongPlayer() {
+  function SongPlayer(Fixtures) {
     /**
-    *@desc SongPlayer object that service returns
+    *@desc currentAlbum object stores album info
     *@type {Object}
     */
-    var SongPlayer = {};
-
-    /**
-    *@desc Current song Object
-    *@type {Object}
-    */
-    var currentSong = null;
+    var currentAlbum = Fixtures.getAlbum();
 
     /**
     *@desc Buzz object audio file
@@ -27,7 +21,7 @@
     var setSong = function(song) {
       if(currentBuzzObject) {
         currentBuzzObject.stop();
-        currentSong.playing = null;
+        SongPlayer.currentSong.playing = null;
       }
 
       currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -35,7 +29,7 @@
         preload: true
       });
 
-      currentSong = song;
+      SongPlayer.currentSong = song;
     };
 
     /**
@@ -49,17 +43,34 @@
     };
 
     /**
+    *@function getSongIndex
+    *@desc Gets index # of current song
+    *@param {Object} song
+    *@return Index # of current song
+    */
+    var getSongIndex = function(song) {
+      return currentAlbum.songs.indexOf(song);
+    };
+
+    /**
+    *@desc Current song Object
+    *@type {Object}
+    */
+    SongPlayer.currentSong = null;
+
+    /**
     *@function SongPlayer.play
     *@desc Plays selected song or paused song
     *@param {Object} song
     */
     SongPlayer.play = function(song) {
-      if(currentSong !== song) {
+      song = song || SongPlayer.currentSong;
+      if(SongPlayer.currentSong !== song) {
         setSong(song);
         playSong(song);
-      } else if (currentSong === song) {
+      } else if (SongPlayer.currentSong === song) {
         if(currentBuzzObject.isPaused()) {
-          currentBuzzObject.play();
+          playSong(song);
         }
       }
     };
@@ -70,8 +81,26 @@
     *@param {Object} song
     */
     SongPlayer.pause = function(song) {
+      song = song || SongPlayer.currentSong;
       currentBuzzObject.pause();
       song.playing = false;
+    };
+
+    /**
+    *@function SongPlayer.previous
+    *@desc Plays previous song in songs list
+    */
+    SongPlayer.previous = function() {
+      var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+      currentSongIndex--;
+      if(currentSongIndex < 0) {
+        currentBuzzObject.stop();
+        SongPlayer.currentSong.playing = null;
+      } else {
+        var song = currentAlbum.songs[currentSongIndex];
+        setSong(song);
+        playSong(song);
+      }
     };
 
     return SongPlayer;
@@ -79,5 +108,5 @@
 
   angular
     .module('blocJams')
-    .factory('SongPlayer', SongPlayer);
+    .factory('SongPlayer', ['Fixtures', SongPlayer]);
 })();
