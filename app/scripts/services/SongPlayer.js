4ec2bd1ff454e28b108225e48ce98ac5888d0467
Deanna Swallow <swallowdeanna@gmail.com>
2017-12-12 14:34:50 -0700

    CP10 Directives2 Completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function(){
-  function SongPlayer(Fixtures) {
+  function SongPlayer($rootScope, Fixtures) {
     /**
     *@desc currentAlbum object stores album info
     *@type {Object}
@@ -38,6 +38,12 @@
         preload: true
       });
 
+      currentBuzzObject.bind('timeupdate', function() {
+        $rootScope.$apply(function() {
+          SongPlayer.currentTime = currentBuzzObject.getTime();
+        });
+      });
+
       SongPlayer.currentSong = song;
     };
 
@@ -66,6 +72,12 @@
     *@type {Object}
     */
     SongPlayer.currentSong = null;
+
+    /**
+    *@desc Current playback time (in seconds) of currently playing song
+    *@type {Number}
+    */
+    SongPlayer.currentTime = null;
 
     /**
     *@function SongPlayer.play
@@ -127,10 +139,21 @@
       }
     };
 
+    /**
+    *@function setCurrentTime
+    *@desc Set current time (in seconds) of currently playing song
+    *@param {Number} time
+    */
+    SongPlayer.setCurrentTime = function(time) {
+      if(currentBuzzObject) {
+        currentBuzzObject.setTime(time);
+      }
+    };
+
     return SongPlayer;
   }
 
   angular
     .module('blocJams')
-    .factory('SongPlayer', ['Fixtures', SongPlayer]);
+    .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
